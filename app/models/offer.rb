d28b3b21830adb3543516721c35440f8dc9b9cc2
Dharam Gollapudi <dharam.gollapudi@gmail.com>
2017-04-02 15:44:51 -0700

    Fix typo

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -9,7 +9,7 @@ class Offer < ActiveRecord::Base
     rate = Rate.new((self.apr.to_f / 100), :apr, :duration => self.term)
     amortization = Amortization.new(self.submission.amount.to_f, rate)
 
-    # calculate the attriutes of this offer
+    # calculate the attributes of this offer
     self.monthly_payment = BigDecimal(amortization.payment.abs.to_s)
     self.total_cost      = BigDecimal(amortization.payments.sum.abs.to_s)
     self.total_interest  = BigDecimal(amortization.interest.sum.to_s)
